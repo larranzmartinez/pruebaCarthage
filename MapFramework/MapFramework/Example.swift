506e04f59f83a89b2983db53898cbc6f98b0b286
Laura Arranz <l.arranz.martinez@accenture.com>
2019-07-16 11:33:43 +0200

    Example.swift

---
+++ MapFramework/MapFramework/Example.swift
@@ -0,0 +1,17 @@
+//
+//  Example.swift
+//  MapFramework
+//
+//  Created by l.arranz.martinez on 16/07/2019.
+//  Copyright © 2019 santander. All rights reserved.
+//
+
+import Foundation
+
+public class Example {
+    
+    public init (){}
+    
+    public func helloWorld(){}
+    
+}
