b52c62f156397b137084fa10682efd18b00ed3ca
Laura Arranz <l.arranz.martinez@accenture.com>
2019-07-16 10:55:28 +0200

    init commit

---
+++ MapFramework/MapFramework/MapFramework.h
@@ -0,0 +1,19 @@
+//
+//  MapFramework.h
+//  MapFramework
+//
+//  Created by l.arranz.martinez on 16/07/2019.
+//  Copyright © 2019 santander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapFramework.
+FOUNDATION_EXPORT double MapFrameworkVersionNumber;
+
+//! Project version string for MapFramework.
+FOUNDATION_EXPORT const unsigned char MapFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapFramework/PublicHeader.h>
+
+
